0f044fcf5f5223b6c2d4d543b7c75c5b517732ed
ahmedhabeeb73 <alnaib888@gmail.com>
2022-08-24 10:41:17 +0200

    add tables

--- database/migrations/2022_08_24_081251_create_convertedvideos_table.php
+++ database/migrations/2022_08_24_081251_create_convertedvideos_table.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('convertedvideos', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('video_id')->references('id')->on('videos')->onDelete('cascade');
+            $table->string('mp4_Format_240')->nullable();
+            $table->string('mp4_Format_360')->nullable();
+            $table->string('mp4_Format_480')->nullable();
+            $table->string('mp4_Format_720')->nullable();
+            $table->string('mp4_Format_1080')->nullable();
+            $table->string('webm_Format_240')->nullable();
+            $table->string('webm_Format_360')->nullable();
+            $table->string('webm_Format_480')->nullable();
+            $table->string('webm_Format_720')->nullable();
+            $table->string('webm_Format_1080')->nullable();
             $table->timestamps();
         });
     }
